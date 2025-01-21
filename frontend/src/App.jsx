d9947d14550c07196ace972c1a813983adeedb7a
Adarsh L K <erlkadarsh@gmail.com>
2025-01-21 23:31:58 +0530

    Frontend Initialized 🚀

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p className="text-2xl text-blue-700">Hello World</p>
+    </div>
+  );
+}
+
+export default App;
